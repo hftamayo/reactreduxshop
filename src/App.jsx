dd1e05dab86c89eac4b323f7fc652720f50d8d5a
hftamayo <hftamayo@gmail.com>
2023-10-13 13:05:50 -0600

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import Header from "./ShoppingCart/Header";
 import Home from "./ShoppingCart/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./ShoppingCart/Checkout";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Home />
+      <Router>
+        <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          ></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
