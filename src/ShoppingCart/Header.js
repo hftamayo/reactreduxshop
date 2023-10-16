5584968f1acf1f8b2d414f624949fdab08a43324
hftamayo <hftamayo@gmail.com>
2023-10-16 12:09:48 -0600

    selector for cart's items

--- src/ShoppingCart/Header.js
+++ src/ShoppingCart/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 
 function Header() {
+  const cart = useSelector(state => state.cart)
   return (
     <div className="header">
       <div className="header_end">
         <Link to="/checkout" style={{textDecoration: "none", color: "white"}}>
           <span>
-            Cart <strong>0</strong>
+            Cart <strong>{cart.cart.length}</strong>
           </span>
         </Link>
       </div>
